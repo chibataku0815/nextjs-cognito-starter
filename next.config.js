058d08daad7a2558052111312983c7034959eb77
chibataku0815 <chiba@fores-tone.co.jp>
2022-05-05 19:19:00 +0900

    Setup componet for layout

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-const path = require('path')
-/** @type {import('next').NextConfig} */
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+    return config
+  },
+  future: {
+    webpack5: true,
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app'
+import Layout from '~/components/layout'
 import { ProvideAuth } from '~/hooks/use-auth'
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <ProvideAuth><Component {...pageProps} /></ProvideAuth>)
+  <ProvideAuth><Layout><Component {...pageProps} /></Layout></ProvideAuth>)
 }
 
 export default MyApp
