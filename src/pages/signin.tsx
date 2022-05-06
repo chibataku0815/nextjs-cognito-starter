3aca8015a829481d702b65d696a1ad194bb5ec75
chibataku0815 <chiba@fores-tone.co.jp>
2022-05-06 13:27:05 +0900

    Setup react-hook-form

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,11 +1,48 @@
+import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+
+interface IFormInputs {
+  firstName: string
+  age: number
+}
+
+const schema = yup
+  .object({
+    firstName: yup.string().required(),
+    age: yup.number().positive().integer().required(),
+  })
+  .required()
 
 const SignIn = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInputs>({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = (data: IFormInputs) => console.log(data)
+
   return (
     <div className="container minH-100 d-flex justify-content-center align-items-center">
       <div className="row">
         <div className="col-12">
-          <input type="text" className="form-control mb-2 mr-sm-2" placeholder="Jane Doe" />
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <input className="form-control" {...register('firstName')} />
+              <p className="text-danger">{errors.firstName?.message}</p>
+            </div>
+            <div className="form-group">
+              <input className="form-control" {...register('age')} />
+            </div>
+            <p className="text-danger">{errors.age?.message}</p>
+
+            <button type="submit" className="btn btn-primary">
+              送信する
+            </button>
+          </form>
         </div>
       </div>
     </div>
