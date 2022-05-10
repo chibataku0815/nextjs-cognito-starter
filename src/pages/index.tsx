16610f96e99599becf03a143cc6ab5d6433fbbf0
chibataku0815 <chiba@fores-tone.co.jp>
2022-05-10 11:29:32 +0900

    Setup yup, yup-password

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,24 @@
-import type { NextPage } from 'next';
-import Link from 'next/link';
-import { useAuth } from '~/hooks/use-auth';
-import PrivateRoute from '../components/PrivateRoute';
+import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useAuth } from '~/hooks/use-auth'
+import PrivateRoute from '../components/PrivateRoute'
 
 const Home: NextPage = () => {
-
-  const auth = useAuth();
+  const auth = useAuth()
 
   if (auth.isLoading) {
-    return <div>ローディング</div>;
+    return <div>ローディング</div>
   }
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      <p>{auth.isAuthenticated ? 'ログイン済' : '未ログイン'}</p>
-      <PrivateRoute>
-        ログインしたので見れます
-      </PrivateRoute>
-      <Link href="/signin">ログイン</Link>
-    </h1>
+    <div className="container">
+      <h1 className="text-3xl font-bold underline">
+        <p>{auth.isAuthenticated ? 'ログイン済' : '未ログイン'}</p>
+        <p>{auth.isAuthenticated}</p>
+        <PrivateRoute>ログインしたので見れます</PrivateRoute>
+        <Link href="/signin">ログイン</Link>
+      </h1>
+    </div>
   )
 }
 
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -2,7 +2,10 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import * as yup from 'yup'
+import YupPassword from 'yup-password'
 import { useAuth } from '~/hooks/use-auth'
+
+YupPassword(yup)
 
 interface IFormInputs {
   username: string
@@ -12,18 +15,28 @@ interface IFormInputs {
 const schema = yup
   .object({
     username: yup.string().required(),
-    password: yup.string().min(8, '8文字以上入力してください').required(),
+    password: yup
+      .string()
+      .minLowercase(8, '8文字以上入力してください')
+      .minUppercase(1, '大文字を1文字以上入力してください')
+      .minSymbol(1, '記号を1文字以上入力してください')
+      .minNumber(1, '数字を1文字以上入力してください')
+      .required(),
   })
   .required()
 
 const SignIn = () => {
   const auth = useAuth()
+
   const {
     register,
     handleSubmit,
+    getValues,
+    watch,
     formState: { errors },
   } = useForm<IFormInputs>({
     resolver: yupResolver(schema),
+    mode: 'onBlur',
   })
   const onSubmit = async (data: IFormInputs) => {
     console.info(JSON.stringify(data))
@@ -34,6 +47,9 @@ const SignIn = () => {
       alert(result.message)
     }
   }
+
+  console.info(watch('password'))
+  console.info(getValues())
 
   return (
     <div className="container minH-100 d-flex justify-content-center align-items-center">
@@ -48,6 +64,10 @@ const SignIn = () => {
               <input className="form-control" {...register('password')} />
             </div>
             <p className="text-danger">{errors.password?.message}</p>
+            <p>文字数{errors.password?.type === 'minLowercase' && !Object.keys(errors).length ? 'NG' : 'OK'}</p>
+            <p>大文字{errors.password?.type === 'minUppercase' ? 'NG' : 'OK'}</p>
+            <p>記号{errors.password?.type === 'minSymbol' ? 'NG' : 'OK'}</p>
+            <p>数字{errors.password?.type === 'minNumber' ? 'NG' : 'OK'}</p>
 
             <button type="submit" className="btn btn-primary">
               送信する
